c1dccf1cea33ff557463cb2ac90f4efa36631680
Jesse Isokääntä <jesseisokaanta@Ohjelmointi-10-MacBook-Pro.local>
2021-10-26 13:25:55 +0300

    test

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    void Start()//start thingy
     {
         
     }
