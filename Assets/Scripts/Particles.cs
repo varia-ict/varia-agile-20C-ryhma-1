0093fe17fc9fb5c48417d170a099edea5e3aa33f
AmandaKankkonen <amandakankkonen@gmail.com>
2021-11-09 09:37:59 +0200

    09/11

---
+++ Assets/Scripts/Particles.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particles : MonoBehaviour {
+
+   ParticleSystem partEffect;
+
+    void Start () 
+    {
+       partEffect = GetComponent<ParticleSystem>();
+    }
+    void Update () 
+    {
+
+    }
+    void OnTriggerEnter (Collider other)
+    {
+        if(other.tag == "Pickup")
+        {
+            partEffect.Play();
+        }
+}
+}
