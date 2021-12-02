6f749cd38ed321e6e903a34beae52bef9ceee9ad
Jesse Nordman <jebu.meme@gmail.com>
2021-12-02 14:10:40 +0200

    Added some code to enemy script

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,23 +6,29 @@ using UnityEngine.UI;
 public class Enemy : MonoBehaviour
 {
     // Variables
-    private Rigidbody enemyRb;
+    public Rigidbody enemyRb;
     public Transform player;
+    public Enemy enemy;
 
     private float dist;
     public float moveSpeed;
     public float howclose;
+    public float hit;
+    public int damage;
+    public int enemyHealth;
+    public bool isAlive;
 
 
     // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
 
     }
 
     // Update is called once per frame
-    void Update()
+    public void Update()
     {
+
         dist = Vector3.Distance(player.position, transform.position);
 
         if (dist <= howclose)
@@ -34,7 +40,25 @@ public class Enemy : MonoBehaviour
         if (dist <= 1.5f)
         {
             // Do damage when close to player
+            
         }
 
     }
+
+    public bool IsDead()
+    {
+        if (enemyHealth != 0)
+        {
+            isAlive = true;
+        }
+
+
+        if (enemyHealth <= 0)
+        {
+            isAlive = false;
+        }
+
+        return false;
+    }
+
 }
